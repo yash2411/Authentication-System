2c66320542332ab957084eba1724b6b35cb63af7
Yash Pandey <yashpandey1106@gmail.com>
2022-05-10 11:59:53 +0530

    Mobile Fields Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:mobileno, :email, :password, :password_confirmation, :remember_me) }
+      devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :mobileno, :email, :password, :remember_me) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:mobileno, :email, :password, :password_confirmation, :current_password) }
+    end
 end
---
+++ db/migrate/20220510060824_add_mobile_to_user.rb
@@ -0,0 +1,5 @@
+class AddMobileToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :mobileno, :integer
+  end
+end
